8ab1270ae97e3492f94d28e4e5d84dc5bbdc92c8
Micko <mickolesmana@gmail.com>
2022-11-30 15:47:04 +0700

    Fixing features_of_interest to handle road data Fixing features_of_interest to handle area gpd Creating file_handler for handling gpd and json fixing path problem in file handler fixing the method geopandas is features_of_interest

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-from upt.utils import make_dir
+from upt.preprocessor.features_of_interest import FeatureOfInterest as foi
+import matplotlib.pyplot as plt
+
+
+depok_obj = foi('Depok',crs=3857,zoom_level=17)
+
+depok_school = depok_obj.area_of_interest({'amenity':'school'})
